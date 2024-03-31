07ec749a75bef248fc2fcfe07aaf592d8538c03c
toroinouesonycat <toroinouesonycat@protonmail.com>
2024-03-30 23:46:52 -0300

    upload first page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,3 +26,26 @@ document.addEventListener('DOMContentLoaded', function() {
         // Aquí podrías implementar una lógica alternativa, como mostrar un botón de reproducción
     });
 });
+
+function copyToClipboard(elementSelector) {
+    var text = document.querySelector(elementSelector).innerText;
+    var elem = document.createElement('textarea');
+    document.body.appendChild(elem);
+    elem.value = text.split(': ')[1]; // Separa el 'CA: ' del resto del texto.
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+
+    // Opcional: Muestra un mensaje de confirmación o cambia el estilo del botón para indicar que se ha copiado.
+}
+
+async function copyToClipboard(elementSelector) {
+    var text = document.querySelector(elementSelector).innerText.split(': ')[1]; // Separa el 'CA: ' del resto del texto.
+    try {
+        await navigator.clipboard.writeText(text);
+        // Opcional: Muestra un mensaje de confirmación o cambia el estilo del botón para indicar que se ha copiado.
+        console.log('Texto copiado al portapapeles');
+    } catch (err) {
+        console.error('Error al copiar texto: ', err);
+    }
+}
